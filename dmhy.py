3d1c43295f3bc271b39fe96895af97ebc5b0a81b
Aythior <conan.ye@gmail.com>
2017-03-16 16:47:12 +0800

    change to class

--- dmhy.py
+++ dmhy.py
@@ -10,76 +10,108 @@ import re
 3、可存储设置
 """
 
+class DMHYDailySpider:
+    dayHead = [r'sunarray', r'monarray', r'tuearray', r'wedarray', r'thuarray', r'friarray', r'satarray']
+    heads = [{"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240'},
+                 {"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'},
+                 {"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'},
+                 {"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'},
+                 {"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Opera/9.80 (Windows NT 6.1) Presto/2.12.388 Version/12.16'},
+                 {"Accept-Language": "zh-CN,zh;q=0.8",
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36'}]
 
-def get_response(url, try_time=1000, **kwargs):
-    if 'session' in kwargs:
-        s = kwargs.pop('session')
-    else:
-        s = requests.Session()
-    r = None
-    while try_time > 0:
+    def __init__(self,url):
+        self.url = url
+        self.titleInfo = []
+        self.titleList = []
+        self.soup = self.get_html_tree(url)
+
+    def get_response(self, url, try_time=1000, **kwargs):
+        if 'session' in kwargs:
+            s = kwargs.pop('session')
+        else:
+            s = requests.Session()
+        r = None
+        while try_time > 0:
+            try:
+                r = s.get(url, **kwargs)
+                break
+            except requests.exceptions.RequestException:
+                try_time -= 1
+        if try_time <= 0:
+            try:
+                s.get(url, **kwargs)
+            except requests.exceptions.RequestException as error:
+                raise Exception(error, "过多的尝试")
+        return r, s
+
+    def get_html_tree(self, url, header=None, params=None):
+        if header is None:
+            header = self.heads[random.randint(0, len(self.heads) - 1)]
+
         try:
-            r = s.get(url, **kwargs)
-            break
-        except requests.exceptions.RequestException:
-            try_time -= 1
-    if try_time <= 0:
+            resp, s = self.get_response(url, params=params, headers=header, cookies={}, timeout=50)
+        except Exception as error:
+            print(error.args[1])
+            exit()
+
+        with open('file.txt', 'w', encoding="utf-8") as f:
+            f.write(resp.text)
+
         try:
-            s.get(url, **kwargs)
-        except requests.exceptions.RequestException as error:
-            raise Exception(error, "过多的尝试")
-    return r, s
+            soup = BeautifulSoup(resp.content, "lxml")
+            return soup
+        except Exception as error:
+            print(error)
+            return
 
-heads = [{"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240'},
-             {"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'},
-             {"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'},
-             {"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'},
-             {"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Opera/9.80 (Windows NT 6.1) Presto/2.12.388 Version/12.16'},
-             {"Accept-Language": "zh-CN,zh;q=0.8",
-              'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36'}]
+    def get_base_data(self):
+        for a in range(7):
+            self.titleInfo.append(re.findall(r'(' + self.dayHead[a] + r'\.push.*<a href="/topics/list\?keyword=.*;)',
+                                        dmhySpider.soup.get_text()))
+
+        for titleOfDay in self.titleInfo:
+            #print(titleOfDay)
+            for title in titleOfDay:
+                self.titleList.append(re.split(r"','", title))
+
+        i = 0
+        while i < self.titleList.__len__():
+            self.titleList[i][0] = self.titleList[i][0][0:self.titleList[i][0].find('.') + 1]
+            groupInfo = re.findall(r'<a href=".*?">.*?</a>', self.titleList[i][3])
+            self.titleList[i][3] = []
+            for group in groupInfo:
+                self.titleList[i][3].append(re.match(r'.*href="(.*?)">(.*?)</a>', group).groups())
+                self.titleList[i][4] = self.titleList[i][4][:self.titleList[i][4].find("']);")]
+            i += 1
+
+        return self.titleList
+
+class TitleGetter:
+    titleList = []
+    def __init__(self, titleList):
+        self.titleList = titleList
+
+    def get_titleList(self):
+        return self.titleList
 
 
-def get_html_tree( url, header=None, params=None):
-    if header is None:
-        header = heads[random.randint(0, len(heads) - 1)]
-    resp, s = get_response(url, params=params, headers=header, cookies={}, timeout=50)
 
-    with open('file.txt', 'w', encoding="utf-8") as f:
-        f.write(resp.text)
 
-    try:
-        soup = BeautifulSoup(resp.content, "lxml")
-        return soup
-    except Exception as error:
-        print(error.args[1])
-        return
+
+
 url = 'https://share.dmhy.org/cms/page/name/programme.html'
 
-soup = get_html_tree(url)
+dmhySpider = DMHYDailySpider(url)
+titleGetter = TitleGetter(dmhySpider.get_base_data())
 
-titleInfo = []
-titleList = []
-dayHead = [r'sunarray',r'monarray',r'tuearray',r'wedarray',r'thuarray',r'friarray',r'satarray']
-for a in range(7):
-    titleInfo.append(re.findall(r'('+dayHead[a]+r'\.push.*<a href="/topics/list\?keyword=.*;)', soup.get_text()))
+soup1 = dmhySpider.get_html_tree('https://share.dmhy.org'+titleGetter.get_titleList()[0][3][0][0])
+list = soup1.find_all('a',{'target','_blank'})
+pass
 
 
-for titleOfDay in titleInfo:
-    print(titleOfDay)
-    for title in titleOfDay:
-        titleList.append(re.split(r"','",title))
-
-i = 0
-while i< titleList.__len__():
-    titleList[i][0] = titleList[i][0][0:titleList[i][0].find('.')+1]
-    groupInfo = re.findall(r'<a href=".*?">.*?</a>',titleList[i][3])
-    titleList[i][3] = []
-    for group in groupInfo:
-        titleList[i][3].append(re.match(r'.*href="(.*?)">(.*?)</a>',group).groups())
-    titleList[i][4] = titleList[i][4][:titleList[i][4].find("']);")]
-    i += 1
